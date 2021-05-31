2dd1adeb18d8aa17a7488cb86f2b404946789ea0
skdlfjl <ok951108@gmail.com>
2021-05-31 23:09:53 +0900

    config test

--- test.py
+++ test.py
@@ -1,3 +1,6 @@
+import sys
+import pdb
+
 def test_logger():
     """Test logger.
 
@@ -13,9 +16,38 @@ def test_logger():
         return False
     return True
 
-if __name__ == '__main__':
-    ret = test_logger()
-    if not ret:
-        raise Exception('Error when test_logger')
-    print('Success - test_logger')
+def test_config():
+    """Test config.
 
+    :return: test result.
+    :rtype: bool
+    """
+    from src import myconfig
+    try:
+        m = myconfig.get_config('/root/da_design_myserver/share/test.config')
+        print('key1=', m['general'].get('key1'))
+        print('key2=', m['general'].get('key2'))
+        print('key3=', m['logger'].get('key3'))
+    except Exception as e:
+        print(e)
+        return False
+    return True
+
+if __name__ == '__main__':
+    target_step = []
+    if len(sys.argv) >= 2:
+        target_step = sys.argv[1].split(',')
+    print('Test steps = ', target_step)
+
+    if not target_step or 'logger' in target_step:
+        ret = test_logger()
+        if not ret:
+            raise Exception('Error when test_logger')
+        print('Success - test_logger')
+
+    if not target_step or 'config' in target_step:
+        ret = test_config()
+        if not ret:
+            raise Exception('Error when test_config')
+        print('Success - test_config')
+    print('Test completed.')
